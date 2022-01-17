7468a7ebdfb9ea122f1d5199b00be3e688effd43
RodolfoPF <rodolfo.pereirafcursos@gmail.com>
2022-01-17 16:47:21 -0300

    Implementação de um algoritmo de ordenação de arrays com a lógica Bublle Sort

---
+++ Ordenação/ordenacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ordenacao.Colecoes;
+
+namespace ordenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] {8,2,4,12,5};
+
+            System.Console.WriteLine("Array Original");
+            op.ImprimirArray(array);
+
+            op.OrdenarBubbleSort(ref array);
+
+            System.Console.WriteLine("Array Ordenado");
+            op.ImprimirArray(array);
+
+        }
+    }
+}
